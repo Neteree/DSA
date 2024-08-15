69faeeca804ddcd250e5857808acbeafd7b120e3
Neteree <57681573+Neteree@users.noreply.github.com>
2024-08-16 00:13:46 +1200

    My new solution after learning about "Two Pointer"

--- ReverseString.cs
+++ ReverseString.cs
@@ -1,5 +1,16 @@
 public class Solution {
     public void ReverseString(char[] s) {
-        Array.Reverse(s);
+        int leftIndex = 0;
+        int rightIndex = s.Length - 1;
+
+        while(leftIndex < rightIndex)
+        {
+            char temp = s[leftIndex];
+            s[leftIndex] = s[rightIndex];
+            s[rightIndex] = temp;
+            
+            leftIndex++;
+            rightIndex--;
+        }
     }
 }
