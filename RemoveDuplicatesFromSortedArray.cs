64ffa89d56e854de3128724c61e8fa7c8d451f50
Neteree <57681573+Neteree@users.noreply.github.com>
2024-08-16 00:01:17 +1200

    This is my solution to the "Remove Duplicates from Sorted Array" problem

---
+++ RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        int leftIndex = 1;
+        
+        for(int rightIndex = 1; rightIndex < nums.Length; rightIndex++)
+        {
+            if(nums[rightIndex] != nums[rightIndex - 1]) {
+                nums[leftIndex] = nums[rightIndex];
+                leftIndex++;
+            }
+        }
+        
+        return leftIndex;
+    }
+}
