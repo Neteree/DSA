48f9c55aff852b721af0d4df339a79f53c8943a9
Neteree <57681573+Neteree@users.noreply.github.com>
2024-08-17 16:52:28 +1200

    Update SortedSquares.cs

--- SortedSquares.cs
+++ SortedSquares.cs
@@ -4,7 +4,7 @@ public class Solution {
         int rightIndex = nums.Length - 1;
         int[] result = new int[nums.Length];
 
-        for(int i = rightIndex; i > -1; i--){
+        for(int i = rightIndex; i > -1; i--) {
             if(Math.Abs(nums[leftIndex]) > Math.Abs(nums[rightIndex])) {
                 result[i] = nums[leftIndex] * nums[leftIndex];
                 leftIndex++;
