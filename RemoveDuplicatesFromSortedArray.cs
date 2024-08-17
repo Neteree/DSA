48a819342e3e60a47f31096847ff6c25e4dea3d3
Neteree <57681573+Neteree@users.noreply.github.com>
2024-08-17 16:49:31 +1200

    Update RemoveDuplicatesFromSortedArray.cs

--- RemoveDuplicatesFromSortedArray.cs
+++ RemoveDuplicatesFromSortedArray.cs
@@ -2,8 +2,7 @@ public class Solution {
     public int RemoveDuplicates(int[] nums) {
         int leftIndex = 1;
         
-        for(int rightIndex = 1; rightIndex < nums.Length; rightIndex++)
-        {
+        for(int rightIndex = 1; rightIndex < nums.Length; rightIndex++) {
             if(nums[rightIndex] != nums[rightIndex - 1]) {
                 nums[leftIndex] = nums[rightIndex];
                 leftIndex++;
