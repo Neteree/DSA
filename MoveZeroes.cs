4a9585ce049b428be66cbc8cc3abcd1ea1a6d8fe
Neteree <57681573+Neteree@users.noreply.github.com>
2024-08-17 16:48:52 +1200

    Update MoveZeroes.cs

--- MoveZeroes.cs
+++ MoveZeroes.cs
@@ -2,10 +2,8 @@ public class Solution {
     public void MoveZeroes(int[] nums) {
         int leftIndex = 0;
 
-        for(int rightIndex = 0; rightIndex < nums.Length; rightIndex++)
-        {
-            if(nums[rightIndex] != 0)
-            {
+        for(int rightIndex = 0; rightIndex < nums.Length; rightIndex++) {
+            if(nums[rightIndex] != 0) {
                 int temp = nums[leftIndex];
                 nums[leftIndex] = nums[rightIndex];
                 nums[rightIndex] = temp;
