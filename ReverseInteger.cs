4d20d5967f5f598b0cae80792060ef62dfdb3c1b
Neteree <57681573+Neteree@users.noreply.github.com>
2024-08-17 16:51:32 +1200

    Update ReverseInteger.cs

--- ReverseInteger.cs
+++ ReverseInteger.cs
@@ -1,13 +1,10 @@
 public class Solution {
-    public int Reverse(int x)
-    {
+    public int Reverse(int x) {
         int result = 0;
         char[] reversedUnsignedXChars = x.ToString().TrimStart('-').Reverse().ToArray();
         
-        if (int.TryParse(reversedUnsignedXChars, out result))
-        {
-            if (x < 0) 
-            {
+        if (int.TryParse(reversedUnsignedXChars, out result)) {
+            if (x < 0) {
                 result = -result;
             }
         }
