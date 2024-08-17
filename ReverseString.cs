fca0d508858868c4976095faf7961e175ef6af78
Neteree <57681573+Neteree@users.noreply.github.com>
2024-08-17 16:51:57 +1200

    Update ReverseString.cs

--- ReverseString.cs
+++ ReverseString.cs
@@ -3,8 +3,7 @@ public class Solution {
         int leftIndex = 0;
         int rightIndex = s.Length - 1;
 
-        while(leftIndex < rightIndex)
-        {
+        while(leftIndex < rightIndex) {
             char temp = s[leftIndex];
             s[leftIndex] = s[rightIndex];
             s[rightIndex] = temp;
