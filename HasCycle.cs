973312722b61c1cbbdacd77b61cceee0a124bd47
Neteree <57681573+Neteree@users.noreply.github.com>
2024-08-22 20:58:43 +1200

    Create HasCycle.cs

---
+++ HasCycle.cs
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool HasCycle(ListNode head) {
+        ListNode slowPtr = head;
+        ListNode fastPtr = head;
+
+        while(fastPtr != null && fastPtr.next != null) {
+            slowPtr = slowPtr.next;
+            fastPtr = fastPtr.next.next;
+
+             if(slowPtr == fastPtr) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
